ba6c24735db7199af242e64c2be572b70c120f70
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-03-16 10:22:26 -0500

    First expectation for DefenseBoard: it gets info about ship placement

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,4 +8,13 @@ describe DefenseBoard do
   it "takes an argument, sets arg to player attribute" do
     expect(DefenseBoard.new(Player.first).player).to eq Player.first
   end
+
+  it "builds hash for occupied coord for player" do
+    occupied_coord = PlayerCoord.create(player_id: 1)
+    occupied_coord.stub(:ship_name) { "Destroyer" }
+    occupied_coord.stub(:coord) { 32 }
+    expect(DefenseBoard.new(Player.first).coords.first)
+    .to eq({ship_name: "Destroyer",
+            coord: 32})
+  end
 end
