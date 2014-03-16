04c260efd7d0c1d59261d9ba73dca6d8a38901a9
mctyn64 <bneuman@unknown949426010a1e.att.net>
2014-03-16 10:24:18 -0500

    Adding #ship_name method to PlayerCoord model

--- app/models/player_coord.rb
+++ app/models/player_coord.rb
@@ -1,4 +1,8 @@
 class PlayerCoord < ActiveRecord::Base
   belongs_to :player 
   belongs_to :ship, class_name: "PlayerShip", foreign_key: :player_ship_id
+
+  def ship_name
+    self.ship.ship.short_name
+  end
 end
